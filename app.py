960c5c5171de5e6ba0fbf681002d8fbb6ff5953b
florencia <florencia.dandrea@gmail.com>
2022-02-18 11:13:46 -0800

    Add example radioitems / checklist

--- app.py
+++ app.py
@@ -1,42 +1,58 @@
 from dash import Dash, html, dcc, Input, Output
 import altair as alt
 from vega_datasets import data
+import pandas as pd
+import numpy as np
 
-
-# Read in global data
-cars = data.cars()
 
 # Setup app and layout/frontend
 app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
 
 app.layout = html.Div([
+    html.H1('RadioItems / Checklist comparison'),
     html.Iframe(
-        id='iframe',
-        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-    dcc.Dropdown(
-        id='xcol-widget',
-        value='Horsepower',  # REQUIRED to show the plot on the first page load
-        options=[{'label': col, 'value': col} for col in cars.columns]),
-    dcc.Dropdown(
-        id='ycol-widget',
-        value='Horsepower',  # REQUIRED to show the plot on the first page load
-        options=[{'label': col, 'value': col} for col in cars.columns])])
+        id = 'iframe',
+        style={'border-width': '0', 
+        'width': '100%',
+        'height': '400px',
+        "top": "50%",
+        "left": "50%",}),
+    html.H3('Checklist'),
+    html.P('The user can visualize multiple options at the same time'),
+    dcc.Checklist(
+        id = 'check',
+        options = ['A', 'B', 'C'], 
+        value = ['A', 'B', 'C']),
+    html.H3('RadioItems'),
+    html.P('The user can select only one option (the options are mutually exclusive )'),
+    dcc.RadioItems(id = 'radio',
+        options = ['set1', 
+                   'set2', 
+                   'tableau10',
+                   'accent' ,
+                   'dark2'],
+        value =  'set1')])
 
 
 # Set up callbacks/backend
-
 @app.callback(
     Output('iframe', 'srcDoc'),
-    Input('xcol-widget', 'value'),
-    Input('ycol-widget', 'value')
+    Input('check', 'value'),
+    Input('radio', 'value')
 )
-def plot_altair(xcol, ycol):
-    chart = alt.Chart(cars).mark_point().encode(
-    x= xcol,
-    y= 'Horsepower',
-    tooltip= ycol).interactive()
-    return chart.to_html()
+def plot_altair(cat, color):
+  np.random.seed(42)
+  source = pd.DataFrame(np.cumsum(np.random.randn(100, 3), 0).round(2),
+                    columns=['A', 'B', 'C'], index=pd.RangeIndex(100, name='x'))
+  source = source.reset_index().melt('x', var_name='category', value_name='y')
+  source = source[source["category"].isin(cat)]
+  line = alt.Chart(source).mark_line(interpolate='basis').encode(
+  x='x:Q',
+  y='y:Q',
+  color='category:N').configure_range(
+  category={'scheme': color})
+  return line.to_html()
 
 
 if __name__ == '__main__':
